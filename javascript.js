5f0cb72dd6e66da6e293486df48a98947432876b
AdrianCo97 <adrian.corazza@outlook.com>
2021-06-21 17:11:54 +0200

    Created a function that changes the header text once the background color is changed.

--- javascript.js
+++ javascript.js
@@ -2,21 +2,31 @@ let clrChanged = new Boolean(false);
 
 let color;
 
+function textChanger(color){
+    $("#h-text").text("The color is now " + color);
+}
+
 //color buttons
 //--------------------------------------------------
 $("#button-red").click(function(){
     $("body").css('background-color', 'red');
     color = "red";
+
+    textChanger(color);
 });
 
 $("#button-green").click(function(){
     $("body").css('background-color', 'green')
     color = "green"
+
+    textChanger(color);
 });
 
 $("#button-dark-grey").click(function(){
     $("body").css('background-color', 'grey')
     color = "grey";
+
+    textChanger(color);
 });
 //--------------------------------------------------
 
